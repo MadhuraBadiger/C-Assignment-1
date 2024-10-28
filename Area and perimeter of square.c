101f3382b940448b7bc653d6e5af2935e97a9490
MadhuraBadiger <madhurabadiger7@gmail.com>
2024-10-28 15:23:57 +0530

    Create Area and perimeter of square.c

---
+++ Area and perimeter of square.c
@@ -0,0 +1,18 @@
+//Code for area and perimeter of square//
+#include<stdio.h>
+int main()
+{
+  float area,peri,a;
+  printf("Enter the value of side:",a);
+  scanf("%f",&a);
+  area=a*a; //formula for area of square
+  printf("\nArea of square=%f sqcm",area);
+  peri=4*a; //formula for perimeter of square//
+  printf("\nPerimeter of square=%f cm", peri);
+}
+
+Output:
+Enter the value of side:6
+
+Area of square=36.000000 sqcm
+Perimeter of square=24.000000 cm
