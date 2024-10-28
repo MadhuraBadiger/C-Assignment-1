3c70713c7961002c91ffa9cdb7d9cf66fe9f71a3
MadhuraBadiger <madhurabadiger7@gmail.com>
2024-10-28 20:47:55 +0530

    Update

---
+++ Variableinteger.c
@@ -0,0 +1,12 @@
+//code for variable integer//
+//Int variable dosen't involve decimal value in it//
+#include<stdio.h> 
+ int main()
+ {
+   int m;
+   m=708;
+   printf("m=%d",m);
+ }
+
+  Output:
+  m=780
