3064b1990c1a01a4d96b26769c870e2ede61253e
MadhuraBadiger <madhurabadiger7@gmail.com>
2024-10-28 15:32:26 +0530

    Create Area and perimeter of triangle.c

---
+++ Area and perimeter of triangle.c
@@ -0,0 +1,21 @@
+//Code for area of triangle//
+#include<stdio.h>
+int main()
+{
+  float area,base,height;
+  printf("Enter the value of base:");
+  scanf("%f",&base);
+  printf("\nEnter the value of height :");
+  scanf("%f",&height);
+  area=0.5*base*height; //formula for area of triangle//
+  printf("\nArea of triangle=%f sqcm",area);
+}
+
+
+Output:
+Enter the value of base:8
+
+Enter the value of height :9.8
+
+Area of triangle=39.200001 sqcm
+
