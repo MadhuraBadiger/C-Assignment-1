4bf3b930964c9d49d8d6bc49e8d4a132fa651c6f
MadhuraBadiger <madhurabadiger7@gmail.com>
2024-10-28 20:36:44 +0530

    Update

---
+++ variablechar.c
@@ -0,0 +1,15 @@
+//code for character program//
+
+#include<stdio.h>
+int main()
+{
+
+     char ch;
+   printf("Challenges are what make life interesting; overcoming them is what makes life meaningful.");
+   
+
+}
+
+
+output:
+Challenges are what make life interesting; overcoming them is what makes life meaningful.
