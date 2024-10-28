f91926ed878f8a82aa38525ccfa87df9bcae6e78
MadhuraBadiger <madhurabadiger7@gmail.com>
2024-10-28 19:50:20 +0530

    Create While loop.c

---
+++ While loop.c
@@ -0,0 +1,24 @@
+//Code for WHILE LOOP//
+#include<stdio.h>
+int main()
+{
+    int m=0;
+    while(m<=10)
+    {
+        printf("\n%d",m);
+        m=m+1;
+    }
+}
+
+Output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
