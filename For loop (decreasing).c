5a8e492b07cec1d175438045457f16447708d20a
MadhuraBadiger <madhurabadiger7@gmail.com>
2024-10-28 19:06:26 +0530

    Create For loop (decreasing).c

---
+++ For loop (decreasing).c
@@ -0,0 +1,27 @@
+//Code for FOR LOOP//
+//Code for decreasing order//
+#include<stdio.h>
+int main()
+{
+    int m;
+    //Code for decreasing order//
+ for(m=10;m>=0;m--)
+    {
+    printf("\n%d",m);
+    }
+
+ 
+}
+
+OUtput:
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
