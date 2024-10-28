360881de957526787b52a4194d79ef59b0d5e199
MadhuraBadiger <madhurabadiger7@gmail.com>
2024-10-28 19:40:29 +0530

    Create For loop (even numbers).c

---
+++ For loop (even numbers).c
@@ -0,0 +1,36 @@
+//Code for FOR LOOP//
+//Code for even numbers//
+#include<stdio.h>
+int main()
+{
+    int m;
+    for(m=1;m<=30;m++)
+    {
+    //even numbers// 
+    
+    if(m%2==0)
+    {
+
+    printf("\n%d",m);
+    }
+    
+    }
+    
+}
+
+Output:
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
