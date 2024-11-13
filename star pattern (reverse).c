01081aa430cc4b69d64705fea90d293ce3be8ad5
MadhuraBadiger <madhurabadiger7@gmail.com>
2024-11-13 20:32:37 +0530

    Create star pattern (reverse).c

---
+++ star pattern (reverse).c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=2;j<=i;j++)
+		{
+			printf("  ");
+		}
+		for(int k=5;k>=i;k--)
+		{
+			printf("  * ");
+			
+		}printf("\n");
+		
+	}
+	
+}
+
+
+Output:
+ *   *   *   *   *
+  *   *   *   *
+    *   *   *
+      *   *
+        *
