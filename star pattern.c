20da716f10e4da2d7774592e788b0d8e357acfde
MadhuraBadiger <madhurabadiger7@gmail.com>
2024-11-13 23:09:55 +0530

    Create star pattern.c

---
+++ star pattern.c
@@ -0,0 +1,23 @@
+//code for star pattern
+#include<stdio.h>
+int main()
+{
+   int i,j;
+   for(i=0;i<=4;i++)
+   {
+   	 for(j=1;j<=i;j++)
+   	 {
+   	   printf("*  "); 	
+	 }
+	 printf("\n ");	
+   
+   } 
+}
+
+
+
+Output:
+ *
+ *  *
+ *  *  *
+ *  *  *  *
